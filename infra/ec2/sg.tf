723d209a90c546c5e785b4912856e4d8d160089b
Markus Winkler <winkler.info@icloud.com>
2025-02-26 15:27:15 +0100

    revert

--- infra/ec2/sg.tf
+++ infra/ec2/sg.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "sg_inet" {
       from_port   = ingress.value.from
       to_port     = ingress.value.to
       protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
   
