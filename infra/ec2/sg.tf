ae85f182b2abfd5f5f5274266a488099dc6a32ad
Markus Winkler <winkler.info@icloud.com>
2025-02-26 15:21:36 +0100

    fix sg_inet

--- infra/ec2/sg.tf
+++ infra/ec2/sg.tf
@@ -34,34 +34,34 @@ resource "aws_security_group" "sg" {
   }
 }
 
-# resource "aws_security_group" "sg_inet" {
-#   for_each    = local.security_groups
-#   name        = "${each.value.name}-inet"
-#   description = each.value.description
-#   vpc_id      = var.vpc_id
+resource "aws_security_group" "sg_inet" {
+  for_each    = local.security_groups
+  name        = "${each.value.name}-inet"
+  description = each.value.description
+  vpc_id      = var.vpc_id
 
-#   dynamic "ingress" {
-#     for_each = each.value.ingress
+  dynamic "ingress" {
+    for_each = each.value.ingress
 
-#     content {
-#       from_port   = ingress.value.from
-#       to_port     = ingress.value.to
-#       protocol    = ingress.value.protocol
-#       cidr_blocks = ["0.0.0.0/0"]
-#     }
-#   }
+    content {
+      from_port   = ingress.value.from
+      to_port     = ingress.value.to
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
   
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = -1
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags = {
-#     Name          = "${each.value.name}-inet"
-#     Environment   = "${var.environment}"
-#     Product       = "playground-one"
-#     Configuration = "nw"
-#   }
-# }
+  tags = {
+    Name          = "${each.value.name}-inet"
+    Environment   = "${var.environment}"
+    Product       = "playground-one"
+    Configuration = "nw"
+  }
+}
